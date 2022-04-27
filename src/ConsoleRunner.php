f26e9cf5960dae89fa4930a48caba7b5dba2d575
Marcin Michalski <evulmastah@gmail.com>
2022-04-27 10:30:28 +0100

    Make ConsoleRunner.setLogger compatible with interface

--- src/ConsoleRunner.php
+++ src/ConsoleRunner.php
@@ -122,7 +122,7 @@ class ConsoleRunner implements LoggerAwareInterface
      *
      * @return void
      */
-    public function setLogger(LoggerInterface $logger)
+    public function setLogger(LoggerInterface $logger): void
     {
         $this->logger = $logger;
     }
